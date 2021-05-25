bc4c7f5953eecdf69bbd9cff3352a2e78b0bd101
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-05-25 13:55:37 +0200

    Update jupyter config to accept gitlab

--- dockerfile/jupyterhub.conf.d/jupyter_config.py
+++ dockerfile/jupyterhub.conf.d/jupyter_config.py
@@ -1,31 +1,45 @@
 # Documentation:
 # - https://oauthenticator.readthedocs.io/en/latest/getting-started.html#gitlab-setup
 
-from oauthenticator.gitlab import GitLabOAuthenticator
-c.JupyterHub.authenticator_class = GitLabOAuthenticator
+import os
 
 # Use Jupyter-Lab interface
 c.Spawner.cmd = ['jupyter-labhub']
 
-# import os
-# if 'GITHUB_CLIENT_ID' in os.environ:
-#     assert 'GITHUB_CLIENT_SECRET' in os.environ
-#     assert 'GITHUB_CALLBACK_URL' in os.environ
-#
-#     # from oauthenticator.github import LocalGitHubOAuthenticator
-#     from oauthenticator.github import GitHubOAuthenticator as Authenticator
-#
-#     c.JupyterHub.authenticator_class = LocalGitHubOAuthenticator
-#
-#     c.LocalGitHubOAuthenticator.create_system_users = True
-#
-#     ## Add the administrator(s) to this list
-#     c.Authenticator.admin_users = {'user'}
-#
-#     ## Add allowed users to this list if you want to restrict access
-#     #c.Authenticator.whitelist = {'joao','maria'}
-#
-#     c.LocalGitHubOAuthenticator.client_id = os.environ['GITHUB_CLIENT_ID']
-#     c.LocalGitHubOAuthenticator.client_secret = os.environ['GITHUB_CLIENT_SECRET']
-#     # c.LocalGitHubOAuthenticator.oauth_callback_url = f'{server_url}/hub/oauth_callback'
-#     c.LocalGitHubOAuthenticator.oauth_callback_url = os.environ['GITHUB_CALLBACK_URL']
+if 'GITHUB_CLIENT_ID' in os.environ:
+    assert 'GITHUB_CLIENT_SECRET' in os.environ
+
+    # from oauthenticator.github import LocalGitHubOAuthenticator
+    from oauthenticator.github import LocalGitHubOAuthenticator
+    c.JupyterHub.authenticator_class = LocalGitHubOAuthenticator
+
+    c.LocalGitHubOAuthenticator.create_system_users = True
+
+    ## Add the administrator(s) to this list
+    c.Authenticator.admin_users = {'chbrandt'}
+
+    ## Add allowed users to this list if you want to restrict access
+    #c.Authenticator.whitelist = {'joao','maria'}
+
+    c.LocalGitHubOAuthenticator.client_id = os.environ['GITHUB_CLIENT_ID']
+    c.LocalGitHubOAuthenticator.client_secret = os.environ['GITHUB_CLIENT_SECRET']
+    c.LocalGitHubOAuthenticator.oauth_callback_url = os.environ['GITHUB_CALLBACK_URL']
+
+else:
+    if 'GITLAB_CLIENT_ID' in os.environ:
+        assert 'GITLAB_CLIENT_SECRET' in os.environ
+
+        from oauthenticator.gitlab import LocalGitLabOAuthenticator
+        c.JupyterHub.authenticator_class = LocalGitLabOAuthenticator
+
+        c.LocalGitLabOAuthenticator.create_system_users = True
+
+        ## Add the administrator(s) to this list
+        c.Authenticator.admin_users = {'chbrandt'}
+
+        ## Add allowed users to this list if you want to restrict access
+        #c.Authenticator.whitelist = {'joao','maria'}
+
+        c.LocalGitLabOAuthenticator.client_id = os.environ['GITLAB_CLIENT_ID']
+        c.LocalGitLabOAuthenticator.client_secret = os.environ['GITLAB_CLIENT_SECRET']
+        c.LocalGitLabOAuthenticator.oauth_callback_url = os.environ['GITLAB_CALLBACK_URL']
